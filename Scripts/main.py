eb893700b9c4bd141e2524f9d06a26c8449486d6
Oakley <Oakley.schofield@gmail.com>
2022-06-02 17:57:23 +0100

    Commenting pt 1 - Main

--- Scripts/main.py
+++ Scripts/main.py
@@ -13,8 +13,6 @@ def startup():
 
 
 
-
-
 def main():
     mouseDown = False
     mouseDownFor = 0
@@ -22,21 +20,29 @@ def main():
     screen = startup()
     clock = pygame.time.Clock()
 
+    # Creates a rendergroup (env.rendergroup)
     renderGroup = env.renderGroup()
-    playBtn = UI.btn(screen, 25, 25, 50, 50, '../images/play-outline.png',
-                     '../images/play-solid.png', '../images/pause-outline.png', '../images/pause-solid.png')
+
 
     # Object creation
+    # Creates a button (UI.btn)
+    playBtn = UI.btn(screen, 25, 25, 50, 50, '../images/play-outline.png',
+                     '../images/play-solid.png', '../images/pause-outline.png', '../images/pause-solid.png')
+    # Defines the points required for a rigidbody (rigidbody.rigidbody)
     point1 = env.point(screen, 80, 130)
-    point25 = env.point(screen, 150, 80)
-    point2 = env.point(screen, 220, 130)
-    point3 = env.point(screen, 200, 200)
-    point4 = env.point(screen, 100, 200)
-    rgb = rb.rigidbody(screen, [point1, point25, point2, point3, point4], 3)
+    point2 = env.point(screen, 150, 80)
+    point3 = env.point(screen, 220, 130)
+    point4 = env.point(screen, 200, 200)
+    point5 = env.point(screen, 100, 200)
+    rgb = rb.rigidbody(screen, [point1, point2, point3, point4, point5], 3)
+
+    # Adds the rigidbody and button to the rendergroup
     renderGroup.rbs.append(rgb)
-    rgb.showColliders(True, True)
     renderGroup.btns.append(playBtn)
 
+    rgb.showColliders(True, True)
+
+    # Floor rigidbody definition
     fl1 = env.point(screen, 0, 970)
     fl2 = env.point(screen, 1500, 970)
     fl3 = env.point(screen, 1500, 1000)
@@ -44,17 +50,20 @@ def main():
     flb = rb.rigidbody(screen, [fl1, fl2, fl3, fl4], 0)
     renderGroup.rbs.append(flb)
 
+    # Event loop
     while 1:
+        # Mouse coords required for the UI
         mousex, mousey = pygame.mouse.get_pos()
+        # Checks current collisions in the rendergroup (env.rendergroup) and if there are, adjustments are calulated.
         c = maths.checkColliders(renderGroup)
         if c is not None:
             print(maths.getColliderAdjustments(screen, c[0], c[1]))
 
         for event in pygame.event.get():
-
+            # Quit via close button
             if event.type == pygame.QUIT:
                 sys.exit()
-
+            # Checks if button is hovered, and changes it, or add a point to the mouses position
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouseDown = True
                 if playBtn.hover:
@@ -65,6 +74,7 @@ def main():
                     else:
                         playBtn.face = 1
                         play = False
+            # Mouse hold cancels point placement and will initiate a line creation
             elif event.type == pygame.MOUSEBUTTONUP:
                 mouseDown = False
                 if mouseDownFor < 80:
@@ -75,13 +85,16 @@ def main():
         if pygame.mouse.get_pressed(num_buttons=3):
             mouseDownFor += 1
 
+        # Screen clear
         screen.fill((255, 255, 255))
 
+        # Botched gravity (TBC)
         if not play:
             rgb.acceleration = (0, 0)
         else:
             rgb.acceleration = (rgb.acceleration[0], -9.81)
 
+        # Render all in the render group, tick the clock, and update the window
         renderGroup.update()
         clock.tick(60)
         pygame.display.update()
